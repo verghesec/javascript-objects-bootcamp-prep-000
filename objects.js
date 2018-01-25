b3ce6c6688efc8e06742bff5cc9f9611007c14bc
Cecil Verghese <cecil.verghese@gmail.com>
2018-01-25 04:11:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = { 3 Doors Down: 'Kryptonite' };
 function updatePlaylist(object, artist, title) {
   object.artist = value;
   return object;
-} 
+}
 
 function removeFromPlaylist(object, artist) {
   delete object.artist;
