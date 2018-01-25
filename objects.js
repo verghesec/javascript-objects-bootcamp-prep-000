3a838a6d553d90e5b9b88be9c747edfcb756daa9
Cecil Verghese <cecil.verghese@gmail.com>
2018-01-25 04:24:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,6 @@
 var playlist = { '3 Doors Down': 'Kryptonite' };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  //playlist.artistName = songTitle;
   return Object.assign({}, playlist, {[artistName]:songTitle});
 }
 
